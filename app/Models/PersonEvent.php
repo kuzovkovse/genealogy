ed1f7715fc8bb2b8c0ade37ff986459ff0a637f9
Stas <staskuzovkov@gmail.com>
2026-02-05 09:58:09 +0300

    Доработана логика событий жизни человека

--- app/Models/PersonEvent.php
+++ app/Models/PersonEvent.php
@@ -22,8 +22,31 @@ class PersonEvent extends Model
         'is_system' => 'boolean',
     ];
 
+    public const TYPES = [
+        'birth'        => ['label' => 'Рождение',            'icon' => '🎂'],
+        'marriage'     => ['label' => 'Брак',                'icon' => '💍'],
+        'child_birth'  => ['label' => 'Рождение ребёнка',    'icon' => '👶'],
+        'move'         => ['label' => 'Переезд',             'icon' => '🏠'],
+        'education'    => ['label' => 'Образование',         'icon' => '🎓'],
+        'service'      => ['label' => 'Служба / война',      'icon' => '🪖'],
+        'death'        => ['label' => 'Смерть',              'icon' => '🕯'],
+        'custom'       => ['label' => 'Другое',              'icon' => '📌'],
+    ];
+
     public function person(): BelongsTo
     {
         return $this->belongsTo(Person::class);
+    }
+
+    public function resolvedIcon(): string
+    {
+        return $this->icon
+            ?? self::TYPES[$this->type]['icon']
+            ?? '📌';
+    }
+
+    public function typeLabel(): string
+    {
+        return self::TYPES[$this->type]['label'] ?? 'Событие';
     }
 }
